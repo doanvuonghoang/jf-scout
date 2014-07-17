15fc7f16caff3d971ab8024358765aa410da0076
Hoàng <doanvuonghoang@gmail.com>
2014-07-18 00:15:33 +0700

    add libs

---
+++ com.jf.scout.commons.libs/com/jf/scout/commons/annotations/InjectExtension.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.jf.scout.commons.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author Hoàng
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InjectExtension {
+}
